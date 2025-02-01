a75ed661abdc5449def497e24aa93d442550f9f6
Justin Nichols <jn@Justins-iMac-2.frontiernet.net>
2025-02-01 17:35:34 -0600

    Initial PWA setup with icons

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "zencora-pwa-v1";
+const urlsToCache = [
+    "/index.html",
+    "/manifest.json",
+    "/icon-512.png",
+    "/icon-192.png"
+];
+
+// Install Service Worker & Cache Resources
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Serve Cached Content When Offline
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Update Cache on Activation
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
